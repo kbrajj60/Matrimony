eeae488fc78cd3f1d4c00355c8c400ee635f0e15
UthraB <kbrajj60@gmail.com>
2020-04-11 22:14:38 +0530

    SimpleMongo

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 
+
 var app = express()
+const uri = "mongodb+srv://bharathi-admin:bharathipassword@sandbox-ile6l.mongodb.net/test";
+
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 
 app.get("/girls",function(req, res){
+    MongoClient.connect(uri, function(err, client) {
+        if(err) {
+             console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+        }
+        console.log('Connected...');
+    });
     res.send("Smethig will be better");
 });
 
